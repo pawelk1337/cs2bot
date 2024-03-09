c7b892b3f1e1dbbb259320a68042a2fc6b7645e1
Paweł <72064903+pawelk1337@users.noreply.github.com>
2024-03-09 12:52:25 +0100

    create a example settings file

---
+++ settings_example.py
@@ -0,0 +1,17 @@
+updateCommands=False
+prefix = "cs!"
+channelNameWhitelist=[
+    "premier",
+    "turniejowy",
+    "quad",
+    "trio",
+    "duo"
+]
+owner_ids = [ # enter your id here
+    967830338116153496
+]
+
+steamWebApiKey="" # not used for now
+webhook=""  # not used for now
+token="" # create a bot here: https://discord.com/developers/applications
+guildID=""
